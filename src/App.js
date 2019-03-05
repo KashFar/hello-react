91d3d8061f71d26af111da5c44c43b5ed960112f
KashF <Kashfarhadi@gmail.com>
2019-03-05 10:43:59 -0500

    changed color to red

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <h1>
-        Hello, <Text name = "Kash" color= "green" />
+        Hello, <Text name = "Kash" color= "red" />
         </h1>
     );
   }
